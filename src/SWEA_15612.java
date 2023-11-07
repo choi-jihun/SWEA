28e2e96f380f5b1ab8ebe61a688f24bf6cd72946
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-08 01:20:30 +0900

    체스판 위의 룩 배치 with 간결한 코드

--- src/SWEA_15612.java
+++ src/SWEA_15612.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
-public class baekjoon_15612 {
+public class SWEA_15612 {
     static int[][] rock = new int[8][8];
 
     public static void main(String[] args) throws IOException {
@@ -66,3 +65,55 @@ public class baekjoon_15612 {
         return cnt == 8;
     }
 }
+
+
+//이런식으로 더 간결하게 짤 수 있다.
+//public class SWEA_15612 {
+//    static int[][] rock = new int[8][8];
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int T = Integer.parseInt(br.readLine());
+//        for (int i = 1; i <= T; i++) {
+//            int totalRocks = 0;
+//            for (int j = 0; j < 8; j++) {
+//                String str = br.readLine();
+//                for (int k = 0; k < 8; k++) {
+//                    rock[j][k] = (str.charAt(k) == 'O') ? 1 : 0;
+//                    totalRocks += rock[j][k];
+//                }
+//            }
+//            if (!checkCol() || !checkRow() || totalRocks != 8) {
+//                sb.append("#").append(i).append(" no\n");
+//            } else {
+//                sb.append("#").append(i).append(" yes\n");
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    static boolean checkRow() {
+//        for (int j = 0; j < 8; j++) {
+//            int cnt = 0;
+//            for (int k = 0; k < 8; k++) {
+//                cnt += rock[j][k];
+//                if (cnt >= 2)
+//                    return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    static boolean checkCol() {
+//        for (int j = 0; j < 8; j++) {
+//            int cnt = 0;
+//            for (int k = 0; k < 8; k++) {
+//                cnt += rock[k][j];
+//                if (cnt >= 2)
+//                    return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
